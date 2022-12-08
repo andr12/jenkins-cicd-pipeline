b82441e5e9c09fda6a11e7177d9cb782591db062
Administrator <admin@example.com>
2022-12-08 22:11:23 +0200

    change

--- gke/variables.tf
+++ gke/variables.tf
@@ -1,7 +1,7 @@
 variable "credentials" {
   type        = string
   description = "Location of the credential keyfile."
-  default = "$JSON_KEY"
+  default = "/home/jenkins/workspace/or2-msq-epm-cedu-t1iylu-05293d904b28.json"
 }
 
 variable "project_id" {
