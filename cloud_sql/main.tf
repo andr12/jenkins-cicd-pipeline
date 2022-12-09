236c2c717625e67861343155317d6f186dc0eb57
Administrator <admin@example.com>
2022-12-09 11:08:05 +0200

    change

--- cloud_sql/main.tf
+++ cloud_sql/main.tf
@@ -3,7 +3,7 @@ resource "random_id" "db_name_suffix" {
 }
 
 resource "google_sql_database_instance" "instance" {
-  name = "var.name${random_id.db_name_suffix.hex}"
+  name = "msql1-${random_id.db_name_suffix.hex}"
   database_version = var.database_version
   region = var.region
   deletion_protection = var.del_protection
