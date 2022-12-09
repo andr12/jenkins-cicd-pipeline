c92ba9eb270a0da43689dea7c01ee634e01d51c6
Administrator <admin@example.com>
2022-12-09 10:42:49 +0200

    change

--- cloud_sql/main.tf
+++ cloud_sql/main.tf
@@ -16,13 +16,13 @@ resource "google_sql_database_instance" "instance" {
 
 resource "google_sql_database" "database" {
   name     = "$MYSQL_DATABASE"
-  instance = google_sql_database_instance.my-instance.name
-  depends_on = [google_sql_database_instance.my-instance]
+  instance = google_sql_database_instance.instance.name
+  depends_on = [google_sql_database_instance.instance]
 }
 
 resource "google_sql_user" "users" {
   name     = "$MYSQL_USER"
-  instance = google_sql_database_instance.my-instance.name
+  instance = google_sql_database_instance.instance.name
   password = "$MYSQL_PASSWORD"
-  depends_on = [google_sql_database_instance.my-instance]
+  depends_on = [google_sql_database_instance.instance]
 }
