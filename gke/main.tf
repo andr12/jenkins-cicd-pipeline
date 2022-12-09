4d0997d20d8c984e9da31b61e38184572e18ba4d
Administrator <admin@example.com>
2022-12-09 10:23:56 +0200

    change

--- gke/main.tf
+++ gke/main.tf
@@ -1,16 +1,16 @@
 resource "google_container_cluster" "primary" {
   name                     = var.name
   project                  = var.project_id
-  location   = var.location
+  location                 = var.location
   network                    = var.network
   subnetwork                 = var.subnetwork
   remove_default_node_pool = true
   initial_node_count       = 1
 } 
- 
+
 resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = "default-node-pool"
-  location   = var.cluster_location
+  location   = var.location
   cluster    = google_container_cluster.primary.name
   node_count = var.nodes_number
 
