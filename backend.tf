49b73aa6ced040270caa69a6975fe16a94ea830e
Administrator <admin@example.com>
2022-12-09 12:48:43 +0200

    change

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "gcs" {
     bucket      = "terraform-halka-bucket"
     prefix      = "terraform/state"
-    credentials = "./or2-msq-epm-cedu-t1iylu-05293d904b28.json"
+    credentials = "$JSON_KEY"
   }
 }
