654fce7194032f09c29ffce84caa85c8fa2117ab
Administrator <admin@example.com>
2022-12-13 20:22:01 +0200

    change

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ module "gke" {
 module "cloud_sql" {
   source            = "./cloud_sql"
   name              = "my-instance"
-  DATABASE_NAME     = "${credentials("MYSQL_DATABASE")}"
-  DATABASE_USER     = "${credentials("MYSQL_USER")}"
-  DATABASE_PASSWORD = "${credentials("MYSQL_PASSWORD")}"
+  DATABASE_NAME     = "${MYSQL_DATABASE}"
+  DATABASE_USER     = "${MYSQL_USER}"
+  DATABASE_PASSWORD = "${MYSQL_PASSWORD}"
  }
 module "artifact_registry" {
   source = "./artifact_registry"
