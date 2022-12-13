2205b530f6b5e0d3e7d42391d977711248f8adae
Administrator <admin@example.com>
2022-12-13 19:50:24 +0200

    change

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ module "gke" {
   name   = "petclinic-cluster"
 }
 module "cloud_sql" {
-  source = "./cloud_sql"
-  name   = "my-instance"
-  DATABASE_NAME     = "${credentials("MYSQL_DATABASE")}"
-  DATABASE_USER     = "${credentials("MYSQL_USER")}"
-  DATABASE_PASSWORD = "${credentials("MYSQL_PASSWORD")}"  
+  source            = "./cloud_sql"
+  name              = "my-instance"
+  DATABASE_NAME     = "${DATABASE_NAME}"
+  DATABASE_USER     = "${DATABASE_USER}"
+  DATABASE_PASSWORD = "${DATABASE_PASSWORD}"
 }
 module "artifact_registry" {
   source = "./artifact_registry"
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,4 @@
 # Project wide variables
-DATABASE_NAME     = "${credentials("MYSQL_DATABASE")}"
-DATABASE_USER     = "${credentials("MYSQL_USER")}"
-DATABASE_PASSWORD = "${credentials("MYSQL_PASSWORD")}"
+DATABASE_NAME     = credentials("MYSQL_DATABASE")
+DATABASE_USER     = credentials("MYSQL_USER")
+DATABASE_PASSWORD = credentials("MYSQL_PASSWORD")
